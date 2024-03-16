7c2b07730c0543e973b4ffe644ea41716b947f41
Oussama Mahjour <mahjouroussama@gmail.com>
2024-03-16 09:42:17 +0000

    set Dockerfile & composer

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__."/../vendor/autoload.php";
+
+use App\Dog;
+
+$dog = new Dog();
+$dog->bark();
+
+
+
+
+
+?>
---
+++ src/Includes/Dog.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+class Dog{
+    public function bark(){
+        echo"barking";
+    }
+}
+
+
+
+?>
+
+
